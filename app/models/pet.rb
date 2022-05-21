e8f2eb8bcb2a44f3d08b999d63b9d5da5761d988
Wesley Garcia <wes@alltradestech.com>
2022-05-20 21:22:41 -0700

    Implement code for Pet#search_results

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -12,4 +12,8 @@ class Pet < ApplicationRecord
   def self.adoptable
     where(adoptable: true)
   end
+
+  def self.search_results(params_query)
+    Pet.where("name ILIKE ?", "%#{params_query}%")
+  end
 end
