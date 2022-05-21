806a2015e3d61e3409c4bcf87b3c295024ef653f
Wesley Garcia <wes@alltradestech.com>
2022-05-20 21:38:45 -0700

    Implement code for User Story:'Searching for Pets for an Application'

--- spec/features/application_forms/show_spec.rb
+++ spec/features/application_forms/show_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationForm, type: :feature do
-  let(:shelter_1) { Shelter.create(name: 'Aurora Shelter', city: 'Aurora, CO', foster_program: false, rank: 5) }
-  let(:shelter_2) { Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: true, rank: 9) }
+  let!(:shelter_1) { Shelter.create(name: 'Aurora Shelter', city: 'Aurora, CO', foster_program: false, rank: 5) }
+  let!(:shelter_2) { Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: true, rank: 9) }
 
-  let(:pet_1) { Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter_1.id) }
-  let(:pet_2) { Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter_1.id) }
-  let(:pet_3) { Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter_2.id) }
+  let!(:pet_1) { Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter_1.id) }
+  let!(:pet_2) { Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter_1.id) }
+  let!(:pet_3) { Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter_2.id) }
 
-  let(:applicationform_1) { ApplicationForm.create(name: "Hank", street_address: "1234 Some Street", city: "Aurora", state: "CO", zip_code: "80015", description: "I'd like an animal please", status: "In Progress") }
-  let(:applicationform_2) { ApplicationForm.create(name: "Levi", street_address: "4321 Another Street", city: "Los Angeles", state: "CA", zip_code: "12345", description: "wanna animal", status: "Pending") }
-  let(:applicationform_3) { ApplicationForm.create(name: "Diana", street_address: "4444 Oneother Court", city: "Detroit", state: "MI", zip_code: "54321", description: "I love animals", status: "Accepted") }
-  let(:applicationform_4) { ApplicationForm.create(name: "Michael", street_address: "621311 Thisdude Street", city: "Philadelphia", state: "PA", zip_code: "19147", description: "Definitely NOT for fighting", status: "Rejected") }
+  let!(:applicationform_1) { ApplicationForm.create(name: "Hank", street_address: "1234 Some Street", city: "Aurora", state: "CO", zip_code: "80015", description: "I'd like an animal please", status: "In Progress") }
+  let!(:applicationform_2) { ApplicationForm.create(name: "Levi", street_address: "4321 Another Street", city: "Los Angeles", state: "CA", zip_code: "12345", description: "wanna animal", status: "Pending") }
+  let!(:applicationform_3) { ApplicationForm.create(name: "Diana", street_address: "4444 Oneother Court", city: "Detroit", state: "MI", zip_code: "54321", description: "I love animals", status: "Accepted") }
+  let!(:applicationform_4) { ApplicationForm.create(name: "Michael", street_address: "621311 Thisdude Street", city: "Philadelphia", state: "PA", zip_code: "19147", description: "Definitely NOT for fighting", status: "Rejected") }
 
-  let(:application_pets_1) { ApplicationPet.create(pet: pet_1, application_form: applicationform_1) }
-  let(:application_pets_2) { ApplicationPet.create(pet: pet_3, application_form: applicationform_2) }
-  let(:application_pets_3) { ApplicationPet.create(pet: pet_1, application_form: applicationform_2) }
+  let!(:application_pets_1) { ApplicationPet.create(pet: pet_1, application_form: applicationform_1) }
+  let!(:application_pets_2) { ApplicationPet.create(pet: pet_3, application_form: applicationform_2) }
+  let!(:application_pets_3) { ApplicationPet.create(pet: pet_1, application_form: applicationform_2) }
 
   describe "Application(form) Show Page" do
     # As a visitor
