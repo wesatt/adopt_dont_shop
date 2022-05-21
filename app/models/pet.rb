1332613bd10ba6358d8c001d8fba7bcee2b4e322
Wesley Garcia <wes@alltradestech.com>
2022-05-21 16:31:28 -0700

    Remove unused Pet#search_results, replace with Pet#search

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -12,8 +12,4 @@ class Pet < ApplicationRecord
   def self.adoptable
     where(adoptable: true)
   end
-
-  def self.search_results(params_query)
-    Pet.where("name ILIKE ?", "%#{params_query}%")
-  end
 end
