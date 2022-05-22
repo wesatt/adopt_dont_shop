d527fe8999e7dfaaea6581ade29486d86b1ed52c
Jim Riddle <jimriddle0@gmail.com>
2022-05-22 12:52:12 -0500

    wrote and passed test for shelters with pending applicaitons

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -2,6 +2,7 @@ class AdminSheltersController < ApplicationController
 
   def index
     @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY shelters.name desc")
+    @shelters_with_pending_apps = Shelter.joins(pets: :application_forms).where(application_forms: {:status => "Pending"})
     # binding.pry
   end
 
--- spec/features/admin/shelter/index_spec.rb
+++ spec/features/admin/shelter/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'the admins shelter index' do
-  it 'lists all the pets with their attributes' do
+  it 'lists all the shelters in reverse alphabetical order' do
     # Admin Shelters Index
     #
     # As a visitor
@@ -29,6 +29,48 @@ RSpec.describe 'the admins shelter index' do
     # save_and_open_page
     expect(shelter_3.name).to appear_before(shelter_2.name)
     expect(shelter_2.name).to appear_before(shelter_1.name)
+
+  end
+
+  it 'lists all the shelters with pending applications' do
+    # Admin Shelters Index
+    #
+    # As a visitor
+    # When I visit the admin shelter index ('/admin/shelters')
+    # Then I see a section for "Shelter's with Pending Applications"
+    # And in this section I see the name of every shelter that has a pending application
+
+    shelter_1 = Shelter.create(name: 'Aurora Shelter', city: 'Aurora, CO', foster_program: false, rank: 5)
+    shelter_2 = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: true, rank: 9)
+    shelter_3 = Shelter.create(name: 'Zaney Building', city: 'Chicago, IL', foster_program: true, rank: 2)
+
+    pet_1 = Pet.create(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: shelter_1.id)
+    pet_2 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter_1.id)
+    pet_3 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter_2.id)
+
+    applicationform_1 = ApplicationForm.create(name: "Hank", street_address: "1234 Some Street", city: "Aurora", state: "CO", zip_code: "80015", description: "I'd like an animal please", status: "In Progress")
+    applicationform_2 = ApplicationForm.create(name: "Levi", street_address: "4321 Another Street", city: "Los Angeles", state: "CA", zip_code: "12345", description: "wanna animal", status: "Pending")
+    applicationform_3 = ApplicationForm.create(name: "Diana", street_address: "4444 Oneother Court", city: "Detroit", state: "MI", zip_code: "54321", description: "I love animals", status: "Accepted")
+    applicationform_4 = ApplicationForm.create(name: "Michael", street_address: "621311 Thisdude Street", city: "Philadelphia", state: "PA", zip_code: "19147", description: "Definitely NOT for fighting", status: "Rejected")
+
+    application_pets_1 = ApplicationPet.create(pet: pet_1, application_form: applicationform_1)
+    application_pets_2 = ApplicationPet.create(pet: pet_3, application_form: applicationform_2)
+    application_pets_3 = ApplicationPet.create(pet: pet_1, application_form: applicationform_2)
+
+    visit "/admin/shelters"
+    # save_and_open_page
+
+    expect(page).to have_content("Shelter's with Pending Applications:")
+    expect(page).to have_content("#{shelter_1.name} has pending applications")
+    expect(page).to have_content("#{shelter_2.name} has pending applications")
+    expect(page).to_not have_content("#{shelter_3.name} has pending applications")
+    # expect("Shelters with Pending Applications:").to appear_before(shelter_2.name)
+    # expect("Shelters with Pending Applications:").to appear_before(shelter_2.name)
+    # expect("Shelters with Pending Applications:").to_not appear_before(shelter_2.name)
+    # expect(shelter_2.name).to appear_after("Shetlers with Pending Applications:")
+    # expect(shelter_3.name).to appear_after!("Shetlers with Pending Applications:")
+    # expect("Admin Page: All Shelters").to appear_before("Shelters with Pending Applications:")
+    # expect("Shelters with Pending Applications:").to appear_before(shelter_2.name)
 
   end
 end
