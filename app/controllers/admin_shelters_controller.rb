0a0d5cb01e05f4eb6b61114e0442ba5e7618ae49
Jim Riddle <jimriddle0@gmail.com>
2022-05-23 10:39:59 -0500

    will clean up controller methods to model methods

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -4,6 +4,7 @@ class AdminSheltersController < ApplicationController
     @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY shelters.name desc")
     @shelters_with_pending_apps = Shelter.joins(pets: :application_forms).where("application_forms.status = 'Pending'").distinct
     # binding.pry
+    #break these out into model methods
   end
 
 end
